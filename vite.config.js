c2fa45891953f464ea250ee158b41a47a69bd312
nevilwan <villeowan@gmail.com>
2026-03-24 06:23:16 +0300

    Design changes for the project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  server: {
+    port: 8080
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
